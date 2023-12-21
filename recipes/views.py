ae659f1ff7b4cf2289c158f0cc752e6a0b91c510
Pedro Henrique Costa Miguez <pcostamiguez@gmail.com>
2023-12-21 20:19:30 -0300

    Sobre and Contato removed

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
@@ -7,11 +6,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'nome': 'Pedro Miguez',
     })
-
-
-def contato(request):
-    return HttpResponse('Contato')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
