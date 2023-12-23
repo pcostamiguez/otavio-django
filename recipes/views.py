bc9db3481832967fe014339a021875b5b8797224
Pedro Henrique Costa Miguez <pcostamiguez@gmail.com>
2023-12-23 09:05:11 -0300

    Template heritage

--- recipes/views.py
+++ recipes/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'nome': 'Daileon 2',
+        'param': 'Daileon 2',
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-detail.html', context={
+        'param': id,
     })
