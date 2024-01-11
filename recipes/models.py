815c33307608cda47980aa3900082dc0a7e65991
Pedro Henrique Costa Miguez <pcostamiguez@gmail.com>
2024-01-11 19:52:05 -0300

    Updated crop algorithm

--- recipes/models.py
+++ recipes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from simple_history.models import HistoricalRecords
+from PIL import Image
+import os
 
 
 def recipe_directory_path(instance, filename):
@@ -45,3 +46,38 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        old_image_path = self.cover.path if self.pk else None
+
+        super().save(*args, **kwargs)
+
+        # Redimensiona a imagem após o upload
+        if self.cover:
+            # Abre a imagem original
+            img = Image.open(self.cover.path)
+
+            # Dimensões desejadas para o crop
+            target_width = 800
+            target_height = 600
+
+            # Redimensiona a imagem para garantir que ela se encaixe nas dimensões desejadas
+            img_resized = img.resize((target_width, target_height), Image.LANCZOS)
+
+            # Obtém as dimensões da imagem redimensionada
+            img_width, img_height = img_resized.size
+
+            # Calcula as coordenadas para cortar a imagem centralizada
+            left = max(0, (img_width - target_width) // 2)
+            top = max(0, (img_height - target_height) // 2)
+            right = min(img_width, left + target_width)
+            bottom = min(img_height, top + target_height)
+
+            # Corta a imagem redimensionada
+            img_cropped = img_resized.crop((left, top, right, bottom))
+
+            # Salva a imagem cortada
+            img_cropped.save(self.cover.path)
+
+        if old_image_path and os.path.exists(old_image_path):
+            os.remove(old_image_path)
